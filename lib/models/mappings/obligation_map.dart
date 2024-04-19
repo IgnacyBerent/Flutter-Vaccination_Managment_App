40a8a8ccaab4b9aa8fe6817d2732bd92b9df4364
IgnacyBerent <2gb02ignac@gmail.com>
2024-04-19 13:24:19 +0200

    alligning obligation to databse

--- lib/models/mappings/obligation_map.dart
+++ lib/models/mappings/obligation_map.dart
@@ -3,5 +3,5 @@ import 'package:vaccination_managment_app/models/types/vaccine_obligation.dart';
 Map obligationMap = {
   'mandatory': VaccineObligation.mandatory,
   'recommended': VaccineObligation.recommended,
-  'nonMandatory': VaccineObligation.nonMandatory,
+  'non_mandatory': VaccineObligation.nonMandatory,
 };
