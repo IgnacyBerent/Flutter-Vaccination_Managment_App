c766103a978a372fe7041d57a7080ee501b25823
IgnacyBerent <2gb02ignac@gmail.com>
2024-04-26 12:31:42 +0200

    Setting up push notifications

--- lib/views/profile/profile_screen.dart
+++ lib/views/profile/profile_screen.dart
@@ -1,18 +1,42 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:vaccination_managment_app/api/auth.dart';
+import 'package:vaccination_managment_app/api/database_api.dart';
 import 'package:vaccination_managment_app/views/my_calendar/my_calendar_screen.dart';
 import 'package:vaccination_managment_app/views/vaccination_history/vaccination_history_screen.dart';
 import 'package:vaccination_managment_app/widgets/buttons/my_icon_button.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
   const ProfileScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    final Authenticate auth = Authenticate();
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
 
+class _ProfileScreenState extends State<ProfileScreen> {
+  final Authenticate _auth = Authenticate();
+  final _db = DatabaseApi();
+
+  void setupPushNotifications() async {
+    final fcm = FirebaseMessaging.instance;
+    await fcm.requestPermission();
+
+    final pushNotificationToken = await fcm.getToken();
+    if (pushNotificationToken != null) {
+      await _db.sendPushNotificationToken(pushNotificationToken);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupPushNotifications();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     void logout() async {
-      await auth.logout();
+      await _auth.logout();
     }
 
     return Column(
