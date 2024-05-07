2717d332abe3cbaa86143c8c35df20dc6f2ec028
IgnacyBerent <2gb02ignac@gmail.com>
2024-05-07 21:27:16 +0200

    Filtering already taken vacicnes

--- lib/views/vaccines_screen/vaccines_screen.dart
+++ lib/views/vaccines_screen/vaccines_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:vaccination_managment_app/models/types/vaccine_status.dart';
 import 'package:vaccination_managment_app/models/vaccine.dart';
+import 'package:vaccination_managment_app/models/vaccine_record.dart';
 import 'package:vaccination_managment_app/views/vaccines_screen/vaccines_screen_elements/vaccine_item.dart';
 import 'package:vaccination_managment_app/api/database_api.dart';
 
@@ -14,10 +16,23 @@ class _VaccinesScreenState extends State<VaccinesScreen> {
   final DatabaseApi _db = DatabaseApi();
   List<Vaccine> vaccines = [];
 
+  Future<List<Vaccine>> _loadVaccines() async {
+    List<Vaccine> fetchedVaccines = await _db.fetchVaccines();
+    List<VaccineRecord> takenVaccines = await _db.fetchVaccineRecords();
+    Set<String> takenVaccineNames = takenVaccines
+        .where((vaccine) => vaccine.status != VaccineStatus.canceled)
+        .map((vaccine) => vaccine.name)
+        .toSet();
+    List<Vaccine> filteredVaccines = fetchedVaccines
+        .where((vaccine) => !takenVaccineNames.contains(vaccine.name))
+        .toList();
+    return filteredVaccines;
+  }
+
   @override
   void initState() {
     super.initState();
-    _db.fetchVaccines().then((value) {
+    _loadVaccines().then((value) {
       setState(() {
         vaccines = value;
       });
